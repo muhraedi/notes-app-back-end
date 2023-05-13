ce859de1a1ce9974c6a6279a4f1c2f864c5ab686
muhraedi <muhraedi@gmail.com>
2023-05-13 17:23:40 +0800

    server-side caching implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -38,9 +38,7 @@ class UsersService {
 
     const result = await this._pool.query(query);
 
-    if (result.rows.length > 0) {
-      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
-    }
+    if (result.rows.length > 0) throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
   }
 
   async getUserById(userId) {
